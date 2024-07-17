78525d9d26cbe77fcc886682dc17171a2e155fb4
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-07-17 17:56:49 +0800

    fixed:(help command works now)

--- dist/bin/typeify.js
+++ dist/bin/typeify.js
@@ -26,13 +26,37 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const converter_1 = require("../converter");
 const path = __importStar(require("path"));
+const printHelp = () => {
+    console.log(`
+Typeify - Convert JavaScript files to TypeScript :D
+
+Usage:
+  typeify <file.js>
+  typeify <folder>/<file.js>
+
+Options:
+  --help    Show this help message
+
+Example:
+  typeify yourfile.js
+`);
+};
 const main = () => {
     const args = process.argv.slice(2);
-    if (args.length === 0) {
-        console.log('Please provide a JavaScript file to convert');
-        process.exit(1);
+    if (args.length === 0 || args.includes("--help")) {
+        printHelp();
+        process.exit(0);
     }
     const filePath = path.resolve(args[0]);
+    const fileExtension = path.extname(filePath).toLowerCase();
+    if (fileExtension === ".ts") {
+        console.log("Oops, that's a TypeScript file. We don't convert those.");
+        process.exit(1);
+    }
+    if (fileExtension !== ".js") {
+        console.log("Please provide a JavaScript (.js) file to convert.");
+        process.exit(1);
+    }
     (0, converter_1.convertFile)(filePath);
 };
 main();
--- src/bin/typeify.ts
+++ src/bin/typeify.ts
@@ -1,17 +1,46 @@
 #!/usr/bin/env node
 
-import { convertFile } from '../converter';
-import * as path from 'path';
+import { convertFile } from "../converter";
+import * as path from "path";
+
+const printHelp = () => {
+  console.log(`
+Typeify - Convert JavaScript files to TypeScript :D
+
+Usage:
+  typeify <file.js>
+  typeify <folder>/<file.js>
+
+Options:
+  --help    Show this help message
+
+Example:
+  typeify yourfile.js
+`);
+};
 
 const main = () => {
-    const args = process.argv.slice(2);
-    if (args.length === 0) {
-        console.log('Please provide a JavaScript file to convert');
-        process.exit(1);
-    }
+  const args = process.argv.slice(2);
 
-    const filePath = path.resolve(args[0]);
-    convertFile(filePath);
+  if (args.length === 0 || args.includes("--help")) {
+    printHelp();
+    process.exit(0);
+  }
+
+  const filePath = path.resolve(args[0]);
+  const fileExtension = path.extname(filePath).toLowerCase();
+
+  if (fileExtension === ".ts") {
+    console.log("Oops, that's a TypeScript file. We don't convert those.");
+    process.exit(1);
+  }
+
+  if (fileExtension !== ".js") {
+    console.log("Please provide a JavaScript (.js) file to convert.");
+    process.exit(1);
+  }
+
+  convertFile(filePath);
 };
 
 main();
